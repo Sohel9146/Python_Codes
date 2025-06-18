b579485328e823707acd732da627f0d07512e7df
Sohel Shaikh <136960137+Sohel9146@users.noreply.github.com>
2025-06-18 14:09:50 +0530

    Fix The Error  02_String.py

--- 02_String.py
+++ 02_String.py
@@ -4,7 +4,7 @@ name_2 = "Sohel Shaikh"
 name_3 = "irfan shaikh"
 name_4 = "ARSHAD SHAIKH"
 name_5 = "Wasim Shaikh"
-name_6 = "shuaib shaikh
+name_6 = "shuaib shaikh"
 
 # name_1.upper()
 # name_1.lower()
